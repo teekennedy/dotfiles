524e8f2f16757130aec7ace5dbf35bcaae5c0892
Terrance Kennedy <terrancerkennedy@gmail.com>
2014-05-28 16:44:43 -0600

    Update Vim submodules

--- arch_install_ycm.sh
+++ arch_install_ycm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd ./.vim/bundle/youcompleteme/cpp
+cd ./.vim/bundle/youcompleteme
+git submodule update --init --recursive
+cd third_party/ycmd/cpp
 cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON
 make ycm_support_libs
