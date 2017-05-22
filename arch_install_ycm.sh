d778fff9bd8664888c63d8dbd53bdbedc3426a8a
Terrance Kennedy <terrancerkennedy@gmail.com>
2017-05-22 13:46:59 -0700

    Remove dwm and arch-specific files

--- arch_install_ycm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ./.vim/bundle/youcompleteme
-git submodule update --init --recursive
-cd third_party/ycmd/cpp
-cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON
-make ycm_support_libs
