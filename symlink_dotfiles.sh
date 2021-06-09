e24a0a96b9911386b2e51e710c9b1a649f3ae704
Terrance Kennedy <terrancerkennedy@gmail.com>
2021-06-09 08:29:53 -0600

    Refactor symlink script

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -5,20 +5,23 @@
 dotfiles_dir=$( cd "$(dirname "$0")" ; pwd -P )/dotfiles
 
 backup_and_symlink() {
+    local src="$dotfiles_dir/$1"
     local dest="$HOME/$1"
-    if [ -L $dest ]; then
-        echo "$dest is already a symlink. Skipping."
+    if [[ -L $dest && "$(readlink $dest)" == "$src" ]]; then
+        echo "[SKP] $dest is already symlinked. Skipping."
         return
     fi
     if [ -e $dest ]; then
-        echo "Backing up $dest to $dest.bak"
-        mv "$dest" "$dest.bak"
+        # man 3 strftime
+        local dest_backup="$dest.$(date "+%Y-%m-%dT%H_%M_%S").bak"
+        echo "[BAK] Backing up $dest to $dest_backup"
+        mv "$dest" "$dest_backup"
     else
 	# ensure parent directory exists
 	mkdir -p $(dirname $dest)
     fi
-    echo "Symlinking $dest to $dotfiles_dir/$1"
-    ln -s "$dotfiles_dir/$1" "$dest"
+    echo "[SYM] Symlinking $dest to $src"
+    ln -s "$src" "$dest"
 }
 
 # Backup and symlink all files in $dotfiles_dir.
