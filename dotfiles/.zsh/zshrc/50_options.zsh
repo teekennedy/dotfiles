ac6a9aaaa4335711445e26f86c3deecbb79859c6
Terrance Kennedy <terrance@missingtoken.net>
2022-01-18 17:21:26 -0700

    Don't set extended_glob

--- dotfiles/.zsh/zshrc/50_options.zsh
+++ dotfiles/.zsh/zshrc/50_options.zsh
@@ -5,9 +5,6 @@
 # Allow comments in interactive shells
 setopt interactive_comments
 
-# Allow extended glob patterns. See man zshexpn for options
-setopt extended_glob
-
 # Don't beep under any circumstance. This is usually disabled by the terminal,
 # but set here for extra insurance.
 setopt no_beep
