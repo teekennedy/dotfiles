264d8b811ad24c13c14f18a16228d5a1d38af89c
Terrance Kennedy <terrance@missingtoken.net>
2024-01-08 20:08:50 -0700

    Setup and configure neotest

--- astro-nvim/.config/nvim/lua/user/plugins/language.lua
+++ astro-nvim/.config/nvim/lua/user/plugins/language.lua
@@ -1,15 +1,11 @@
 return {
-	-- AstroCommunity language packs
-	-- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack
-	{ import = "astrocommunity.pack.bash" },
-	{ import = "astrocommunity.pack.go" },
-	{ import = "astrocommunity.pack.json" },
-	{ import = "astrocommunity.pack.lua" },
-	{ import = "astrocommunity.pack.markdown" },
-	{ import = "astrocommunity.pack.terraform" },
-	{ import = "astrocommunity.pack.yaml" },
-
-	-- NeoTest (integrates with go, python, rust)
-	-- https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/test/neotest/init.lua
-	{ import = "astrocommunity.test.neotest" },
+  -- AstroCommunity language packs
+  -- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.yaml" },
 }
---
+++ astro-nvim/.config/nvim/lua/user/plugins/neotest.lua
@@ -0,0 +1,82 @@
+-- NeoTest (integrates with go, python, rust)
+-- https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/test/neotest/init.lua
+local utils = require "astronvim.utils"
+local prefix = "<leader>t"
+local icon = vim.g.icons_enabled and "󰙨 " or ""
+return {
+  -- Disable toggleterm.nvim so I can use <leader>t prefix for tests instead
+  { "akinsho/toggleterm.nvim", enabled = false },
+  {
+    "nvim-neotest/neotest",
+    ft = { "go", "rust", "python", "lua" },
+    dependencies = {
+      "nvim-neotest/neotest-go",
+      "nvim-neotest/neotest-python",
+      "rouge8/neotest-rust",
+      -- Add Neotest as a Neodev plugin (lua specific)
+      {
+        "folke/neodev.nvim",
+        opts = function(_, opts)
+          opts.library = opts.library or {}
+          if opts.library.plugins ~= true then
+            opts.library.plugins = require("astronvim.utils").list_insert_unique(opts.library.plugins, "neotest")
+          end
+          opts.library.types = true
+        end,
+      },
+    },
+    opts = function()
+      return {
+        adapters = {
+          require "neotest-go",
+          require "neotest-rust",
+          require "neotest-python",
+        },
+        -- Allow INFO-level diagnostic messages
+        diagnostic = {
+          severity = vim.diagnostic.severity.INFO,
+        },
+      }
+    end,
+    config = function(_, opts)
+      local neotest = require "neotest"
+      local mappings = {
+        n = {
+          [prefix] = { desc = icon .. "Test" },
+          [prefix .. "f"] = { function() neotest.run.run(vim.fn.expand "%") end, desc = "Test current file" },
+          [prefix .. "a"] = {
+            function() neotest.run.run(vim.fn.expand "%") end,
+            desc = "Run all tests in current directory",
+          },
+          [prefix .. "w"] = {
+            function() neotest.watch.toggle(vim.fn.expand "%") end,
+            desc = "Toggle watching current file",
+          },
+          [prefix .. "W"] = {
+            function() neotest.watch.toggle(vim.fn.getcwd()) end,
+            desc = "Toggle watching current directory",
+          },
+          [prefix .. "c"] = { function() neotest.run.run() end, desc = "Run closest test" },
+          [prefix .. "S"] = { function() neotest.run.stop() end, desc = "Stop current test run" },
+          [prefix .. "s"] = { function() neotest.summary.toggle() end, desc = "Toggle summary panel" },
+          [prefix .. "o"] = { function() neotest.output_panel.toggle() end, desc = "Toggle output panel" },
+        },
+      }
+      if utils.is_available "nvim-dap" then
+        mappings.n[prefix .. "d"] = { function() neotest.run.run { strategy = "dap" } end, desc = "Debug closest test" }
+      end
+      utils.set_mappings(mappings)
+      -- get neotest namespace (api call creates or returns namespace)
+      local neotest_ns = vim.api.nvim_create_namespace "neotest"
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+      neotest.setup(opts)
+    end,
+  },
+}
