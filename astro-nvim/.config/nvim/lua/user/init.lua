816e40adee5c0d7d9fa4b6d77a9277cf6807407a
Terrance Kennedy <terrance@missingtoken.net>
2024-01-25 14:20:13 -0700

    NeoVim: Disable format-on-write for json files

--- astro-nvim/.config/nvim/lua/user/init.lua
+++ astro-nvim/.config/nvim/lua/user/init.lua
@@ -38,6 +38,7 @@ return {
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           "yaml",
+          "json",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
