101ad76aabaa38604f45fef8b54e1525bd1460e6
Terrance Kennedy <terrance@missingtoken.net>
2024-02-13 12:20:55 -0700

    zshrc: Update path to powerlevel10k

--- p10k/.zsh/zshrc/85_p10k_load.zsh
+++ p10k/.zsh/zshrc/85_p10k_load.zsh
@@ -1,4 +1,4 @@
 # Load Powerlevel10k zsh theme from homebrew
-p10k_zsh_theme="$(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme"
+p10k_zsh_theme="$(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme"
 [ -f $p10k_zsh_theme ] && source $p10k_zsh_theme
 unset p10k_zsh_theme
