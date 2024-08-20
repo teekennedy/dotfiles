0e3ef6ad882b60917f17334cc58567418d963649
Terrance Kennedy <terrance@missingtoken.net>
2024-08-20 16:30:47 -0600

    nvim: Add debug support for go via nvim-dap

--- astro-nvim/.config/nvim/lua/plugins/language.lua
+++ astro-nvim/.config/nvim/lua/plugins/language.lua
@@ -8,4 +8,22 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.pack.yaml" },
+  {
+    "mfussenegger/nvim-dap",
+    setup = {
+      -- Additional dap configurations can be added.
+      -- dap_configurations accepts a list of tables where each entry
+      -- represents a dap configuration. For more details do:
+      -- :help dap-configuration
+      dap_configurations = {
+        {
+          -- Must be "go" or it will be ignored by the plugin
+          type = "go",
+          name = "Attach remote",
+          mode = "remote",
+          request = "attach",
+        },
+      },
+    },
+  },
 }
