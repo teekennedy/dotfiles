82f0dbc0a684458e8beb1f5e7608d046a585dab3
Terrance Kennedy <terrance@missingtoken.net>
2025-05-06 17:42:08 -0600

    nvim: Update plugins, better copilot integration

--- astro-nvim/dot-config/nvim/lua/plugins/codecompanion.lua
+++ astro-nvim/dot-config/nvim/lua/plugins/codecompanion.lua
@@ -1,19 +1,37 @@
 ---@type LazySpec
 return {
-	{
-		"olimorris/codecompanion.nvim",
-		config = true,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		opts = {
-			strategies = {
-				-- Change the default chat adapter
-				chat = {
-					adapter = "anthropic",
-				},
-			},
-		},
-	},
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      {
+        "zbirenbaum/copilot.lua",
+        opts = {
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+        },
+      },
+      -- Integrate copilot with cmp
+      {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+          { "zbirenbaum/copilot-cmp", config = true },
+        },
+        ---@param opts cmp.ConfigSchema
+        opts = function(_, opts) table.insert(opts.sources, { name = "copilot" }) end,
+      },
+    },
+    opts = {
+      strategies = {
+        -- Change the default chat adapter
+        chat = {
+          adapter = "copilot",
+        },
+        inline = {
+          adapter = "copilot",
+        },
+      },
+    },
+  },
 }
