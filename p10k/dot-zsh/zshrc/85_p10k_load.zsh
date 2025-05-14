3a61f66aaa224fea4855f5d8b2755180c075713c
Terrance Kennedy <terrance@missingtoken.net>
2025-05-14 17:21:30 -0600

    Put guards around p10k

--- p10k/dot-zsh/zshrc/85_p10k_load.zsh
+++ p10k/dot-zsh/zshrc/85_p10k_load.zsh
@@ -1,4 +1,6 @@
 # Load Powerlevel10k zsh theme from homebrew
-p10k_zsh_theme="$(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme"
-[ -f $p10k_zsh_theme ] && source $p10k_zsh_theme
-unset p10k_zsh_theme
+if command_exists brew; then
+  p10k_zsh_theme="$(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme"
+  [ -f $p10k_zsh_theme ] && source $p10k_zsh_theme
+  unset p10k_zsh_theme
+fi
