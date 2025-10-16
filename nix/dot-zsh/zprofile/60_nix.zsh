2e08c1da0b19b70f41f5b5d66991dd1837460aed
Terrance Kennedy <terrance@missingtoken.net>
2025-10-16 09:19:57 -0600

    Migrate nix config to zprofile; update VS Code settings

--- nix/dot-zsh/zprofile/60_nix.zsh
+++ nix/dot-zsh/zprofile/60_nix.zsh
@@ -2,6 +2,11 @@ if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
 
+# Export SSL_CERT_FILE so nix-managed curl and openssl can pick it up
+if [ -e "/etc/nix/macos-keychain.crt" ]; then
+  export SSL_CERT_FILE="/etc/nix/macos-keychain.crt"
+fi
+
 # home manager (with homeManager.useUserPackages = true)
 if [ -e "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh" ]; then
   . "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
@@ -17,9 +22,4 @@ if [ -e "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh" ]; then
       ;;
     esac
   fi
-fi
-
-# Export SSL_CERT_FILE so nix-managed curl and openssl can pick it up
-if [ -e "/etc/nix/macos-keychain.crt" ]; then
-  export SSL_CERT_FILE="/etc/nix/macos-keychain.crt"
 fi
