13685c63fd00e11a124b587d6b6c9a69c93309f4
Terrance Kennedy <terrance@missingtoken.net>
2026-02-03 10:36:53 -0700

    Switch from codecompanion to avante

---
+++ astro-nvim/dot-config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,52 @@
+---@type LazySpec
+return {
+  {
+    "yetone/avante.nvim",
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has "win32" ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      or "make",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- add any opts here
+      -- this file can contain specific instructions for your project
+      instructions_file = "agents.md",
+      provider = "copilot",
+      disabled_tools = { "web_search" },
+      providers = {
+        copilot = {
+          model = "claude-opus-4.5",
+          timeout = 30000, -- Timeout in milliseconds
+          -- extra_request_body = {
+          --   temperature = 0.75,
+          --   max_tokens = 20480,
+          -- },
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "Kaiser-Yang/blink-cmp-avante", -- blink.cmp integration
+      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      -- "stevearc/dressing.nvim", -- for input provider dressing
+      -- "folke/snacks.nvim", -- for input provider snacks
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
