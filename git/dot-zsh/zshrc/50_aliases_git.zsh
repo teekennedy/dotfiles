dab2883c507d277bad0ba6cbb1d3862f4eb725a6
Terrance Kennedy <terrance@missingtoken.net>
2026-06-15 12:29:55 -0600

    Add / update git aliases

--- git/dot-zsh/zshrc/50_aliases_git.zsh
+++ git/dot-zsh/zshrc/50_aliases_git.zsh
@@ -2,15 +2,23 @@
 
 # Alias some common git commands
 if command_exists git; then
+  # gb = Git Branch
+  alias gb='git branch'
+  # gd = Git Diff
+  alias gd='git diff'
+  # gdc = Git Diff Cached
+  alias gdc='git diff --cached'
   # gs = Git Status
   alias gs='git status'
+  # gup = Git Update
+  alias gup='git update'
   # gfa = Git Fetch All
   alias gfa='git fetch --all --prune'
-  # gca = Git Commit Amend
-  alias gca='git commit --amend --no-edit'
+  # gcan = Git Commit Amend No edit
+  alias gcan='git commit --amend --no-edit'
   # gcap = Git Commit Amend Push
   alias gcap='git commit --amend --no-edit && git push --force'
-  # gca = Git Add modified Commit Amend
+  # gaca = Git Add modified Commit Amend
   alias gaca='git add -u && git commit --amend --no-edit'
   # gcap = Git Add modified Commit Amend Push
   alias gacap='git add -u && git commit --amend --no-edit && git push --force'
