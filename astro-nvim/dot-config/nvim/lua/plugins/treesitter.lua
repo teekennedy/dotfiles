a4430ea7a253fed6f5703bb8a7337c6d570d766b
Terrance Kennedy <terrance@missingtoken.net>
2026-08-21 23:05:50 -0600

    Feat: neovim: add keymaps for treesitter textobjects

--- astro-nvim/dot-config/nvim/lua/plugins/treesitter.lua
+++ astro-nvim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -4,13 +4,52 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  opts = {
-    treesitter = {
-      ensure_installed = {
-        "jsonnet",
-        -- add more arguments for adding more treesitter parsers
-      },
-    },
-  },
+	"AstroNvim/astrocore",
+	opts = {
+		treesitter = {
+			ensure_installed = {
+				"jsonnet",
+				-- add more arguments for adding more treesitter parsers
+			},
+			textobjects = {
+				select = {
+					select_textobject = {
+						["af"] = { query = "@function.outer", desc = "around function" },
+						["if"] = { query = "@function.inner", desc = "around function" },
+					},
+				},
+				move = {
+					goto_next_start = {
+						["]f"] = { query = "@function.outer", desc = "Next function start" },
+					},
+					goto_next_end = {
+						["]F"] = { query = "@function.outer", desc = "Next function end" },
+					},
+					goto_previous_start = {
+						["[f"] = {
+							query = "@function.outer",
+							desc = "Previous function start",
+						},
+					},
+					goto_previous_end = {
+						["[F"] = {
+							query = "@function.outer",
+							desc = "Previous function end",
+						},
+					},
+				},
+				swap = {
+					swap_next = {
+						[">F"] = { query = "@function.outer", desc = "Swap next function" },
+					},
+					swap_previous = {
+						["<F"] = {
+							query = "@function.outer",
+							desc = "Swap previous function",
+						},
+					},
+				},
+			},
+		},
+	},
 }
