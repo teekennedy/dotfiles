7f176dbbacceab12b83200d8f80581a78b50ac15
Terrance Kennedy <terrance@missingtoken.net>
2026-09-03 13:15:58 -0600

    Feat: AstroNvim: Add DiffView plugin, update deps

--- astro-nvim/dot-config/nvim/lua/plugins/git.lua
+++ astro-nvim/dot-config/nvim/lua/plugins/git.lua
@@ -1,5 +1,8 @@
 return {
-  -- Open in GitHub
-  -- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/git/openingh-nvim
-  { import = "astrocommunity.git.openingh-nvim" },
+	-- Open in GitHub
+	-- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/git/openingh-nvim
+	{ import = "astrocommunity.git.openingh-nvim" },
+	-- DiffView
+	-- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/git/diffview-nvim
+	{ import = "astrocommunity.git.diffview-nvim" },
 }
